4ef569295261a4a86427639338125fe7a976f011
sardor-home <usardor.dev@gmail.com>
2025-03-12 02:45:08 +0500

    stateless element.markneedsBuild(stateless statefull widget)

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:playground/home_page.dart';
-import 'package:playground/inherited_notifier.dart';
 
 class MiniApp extends StatelessWidget {
   const MiniApp({super.key});
@@ -9,15 +8,10 @@ class MiniApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     log('MiniApp got (re)build');
-    return SettingsInheritedModifier(
-      notifier: ValueNotifier(
-        AppSettings(name: 'Sardor', isDark: false, language: 'English'),
-      ),
-      child: const MaterialApp(
-        title: 'Test App',
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
-      ),
+    return const MaterialApp(
+      title: 'Test App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,45 +1,49 @@
-import 'dart:developer';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:playground/inherited_notifier.dart';
-import 'package:playground/settings_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    final model = SettingsInheritedModifier.of(context).value;
-    log('HomePage got (re)build');
+    final Random random = Random();
+    int counter = 0;
     return Scaffold(
+      backgroundColor: Color.fromRGBO(
+        random.nextInt(256), // Red (0-255)
+        random.nextInt(256), // Green (0-255)
+        random.nextInt(256), // Blue (0-255)
+        1, // Opacity (1 = fully opaque)
+      ),
       appBar: AppBar(
-        title: const Text('Current state of the app'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ListTile(
-              title: const Text('Language:'),
-              trailing: Text(model.language),
-            ),
-            ListTile(
-              title: const Text('Theme mode:'),
-              trailing: Text(model.isDark ? "dark" : 'light'),
-            ),
-            ListTile(title: const Text('Name:'), trailing: Text(model.name)),
-            TextButton(
-              onPressed: () async => await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SettingsPage(),
-                ),
-              ),
-              child: const Text('Settings page'),
-            )
-          ],
+        backgroundColor: Color.fromRGBO(
+          random.nextInt(256), // Red (0-255)
+          random.nextInt(256), // Green (0-255)
+          random.nextInt(256), // Blue (0-255)
+          1, // Opacity (1 = fully opaque)
         ),
+        title: const Text('Stateless widget but statefull'),
       ),
+      body: Builder(builder: (context) {
+        return Center(
+          child: Column(
+            children: [
+              Spacer(),
+              Spacer(),
+              Text('You\'ve pressed: $counter times'),
+              Spacer(),
+              TextButton(
+                onPressed: () async {
+                  counter++;
+                  (context as Element).markNeedsBuild();
+                },
+                child: const Text('Settings page'),
+              ),
+              Spacer(),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
